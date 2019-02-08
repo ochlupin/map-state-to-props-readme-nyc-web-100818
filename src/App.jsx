f5aed7764bb7e2897ba49f838e71159ceeb5be11
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2019-02-08 13:07:26 +0700

    Done.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+class App extends Component {
+  handleOnClick() {
+    this.props.dispatch({
+      type: 'INCREASE_COUNT'
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={event => this.handleOnClick()}>Click</button>
+        <p>{this.props.items.length}</p>
+      </div>
+    );
+  }
+}
+
+// Our goal is to:
+// 1. Only re-render our App component when specific changes to state occur
+// 2. Only provide the slice of state that we need to our App component
+// We need :
+// 1. A function that listens to every change in the store - connect listens to each change in state that occurs.
+// 2. Filters out the changes relevant to a particular component - When a change occurs. the connect function calls a function mapStateTopProps we specify exactly which slice of the state we want to provide to our component  (i.e. state.items)
+// 3. Provide to that component - then we specify which component we are providing to i.e. App as in connect(mapStateToProps)(App). FInally the entire connect() method returns a new component and this is the component we wish to export
+
+const mapStateToProps = state => {
+  return { items: state.items };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'; // import provider from React-Redux
+
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
@@ -11,6 +13,8 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
